d46633486f225457ed7395645736dac14521d0b9
Marina <sheremetamaryna@gmail.com>
2022-11-06 21:28:55 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,60 @@
-import React from "react";
-import "./feedback/Feedback.css";
-import FeedbackOptions from "./feedback/FeedbackOptions";
-import Statistics from "./feedback/Statistics";
-import Section from "./feedback/Section";
+import React from 'react';
+import './feedback/Feedback.css';
+import FeedbackOptions from './feedback/FeedbackOptions';
+import Statistics from './feedback/Statistics';
+import Section from './feedback/Section';
 
-
-class App extends React.Component { 
-  state = { 
+class App extends React.Component {
+  state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
   };
-   onLeaveFeedback = (event) => {
+  onLeaveFeedback = event => {
     console.log(event);
     this.setState(prevState => ({
-        [event.target.name]: prevState[event.target.name] + 1
-    }))
-}
-
+      [event.target.name]: prevState[event.target.name] + 1,
+    }));
+  };
 
   countTotalFeedback = () => {
-      return this.state.good + this.state.neutral + this.state.bad;
-  }   
-    countPositiveFeedbackPercentage = () => {
-      return this.countTotalFeedback() === 0 ? '0.00' : (100 * this.state.good / this.countTotalFeedback()).toFixed(2);
-   }
-      
-  
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback() === 0
+      ? '0.00'
+      : ((100 * this.state.good) / this.countTotalFeedback()).toFixed(2);
+  };
 
-  render () {
-   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'block',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-            <Section title="Please leave feedback">
-            <FeedbackOptions
-            onLeaveFeedback={this.onLeaveFeedback}>
-           </FeedbackOptions>
-           </Section>
-            <Section title="Statistics">
- 
-              
-            <Statistics 
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'block',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          <Statistics
             good={this.state.good}
-             neutral={this.state.neutral} 
-             bad={this.state.bad}
-              total={this.countTotalFeedback()} 
-              positivePercentage={this.countPositiveFeedbackPercentage()}>
-              </Statistics>
-            </Section>
-                 
-    </div>
-    
-  );
-    }
-
-};
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+        </Section>
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/feedback/FeedbackOptions.jsx
+++ src/components/feedback/FeedbackOptions.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React from 'react';
 
-
-const FeedbackOptions =({ onLeaveFeedback}) => (
-
-            <div>
-            <button type = "button" className="Feedback__button" name="good" onClick={onLeaveFeedback}>Good</button>
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map(option => {
+      return (
+        <button
+          type="button"
+          className="Feedback__button"
+          name={option}
+          onClick={onLeaveFeedback}
+        >
+          {option}
+        </button>
+      );
+    })}
+    {/* <button type = "button" className="Feedback__button" name="good" onClick={onLeaveFeedback}>Good</button>
             <button type = "button" className="Feedback__button" name="neutral" onClick={onLeaveFeedback}>Neutral</button>
-            <button type = "button" className="Feedback__button" name="bad" onClick={onLeaveFeedback}>Bad</button>
-
-            </div>
-      
-    );
-    export default FeedbackOptions;
+            <button type = "button" className="Feedback__button" name="bad" onClick={onLeaveFeedback}>Bad</button> */}
+  </div>
+);
+export default FeedbackOptions;
