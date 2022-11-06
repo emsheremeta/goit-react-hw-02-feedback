fb73119b0e0bfcd15d9339b88f36e0d089bf0fcd
Marina <sheremetamaryna@gmail.com>
2022-11-06 23:21:02 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import PhoneApp from 'components/PhoneApp'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App/>
-    <PhoneApp/>
   </React.StrictMode>
 );
